b0333a9730c21cdd8726383b3e43fe07c2d93b04
MrLoh <flojoto@hotmail.com>
2015-03-17 15:59:18 +0100

    creat short data samples
    Former-commit-id: 8e0bb3caab45a4b1e37770860f1d5cd0f908a293

--- LocationMapping/Trackpoint.java
+++ LocationMapping/Trackpoint.java
@@ -1,24 +1,95 @@
+import java.lang.Object
+
+
 class Trackpoint {
     // Attribute
-    Location location;
-    Timestamp timestamp;
+    /**
+    * Ortsinformation, enthält doubles für Längen und Breitengrad
+    */
+    private Location location;
+    /**
+    * Timestamp
+    */
+    private Timestamp timestamp;
+    /**
+    * Identifikationsnummer für den Datensatz, per Default auf 0 gesetzt
+    */
+    private int id = 0;
+    /**
+    * Label, welcher von User festgesetzt werden kann, leer bei default
+    */
+    private String label = "";
 
-    // Konstruktor
-    public Trackpoint(int year, int month, int date, int hour, int minute, int second, double longitude, double lattitude){
-        timestamp = new Timestamp(year, month, date, hour, minute,second, 0);
-        location = new Location(longitude, latitude);
-    };
+    /**
+    *
+    */
+    public Trackpoint(int id, Timestamp timestamp, Location location){
+        this.timestamp = timestamp;
+        this.id = id;
+        this.location = new Location(location);
+    }
+
+    // Konstruktor ohne id
+    public Trackpoint(Timestamp timestamp, Location location){
+        this.timestamp = timestamp;
+        this.location = new Location(location);
+    }
+
+    // Set ID
+    void setId(int id){
+        this.id = id;
+    }
+
+    // Get ID
+    int getId(){
+        return id;
+    }
+
+    // Set Label
+    void setLabel(String label){
+        this.label = label;
+    }
+
+    // Get Label
+    public String getLabel(){
+        return label;
+    }
 
     // Time Methoden
-    double getTimestamp();
-    String getDateTime();
-    int compareTimeTo();
+    public Timestamp getTimestamp(){
+        return timestamp;
+    }
+
+    //Ausgabe Datum und Uhrzeit als String
+    public String getDateTime(){
+        return timestamp.toString();
+    }
+
+    // Zeitdifferenz zweier Trackpoints in Sekunden.
+    public long compareTimeTo(Trackpoint trackpoint){
+        return Math.abs(this.timestamp.getTime() - trackpoint.getTimestamp().getTime());
+     }
 
     // Location Methoden
-    double getLongitude();
-    double getLattitude();
-    boolean equalLocation(); // epsilon bei GPS??
+    public Location getLocation(){
+        return location;
+    }
 
-    String toString();
-    boolean equals(); // epsilon bei GPS und Time??
+    // Test, ob 2 Trackpoints bis auf die angegebene Toleranzgrenze übereinstimmen
+    public boolean equalLocation(double toleranceRange, Trackpoint trackpoint){
+        double distance = this.location.distanceTo(trackpoint.getLocation());
+        return (distance <= toleranceRange);
+    }
+
+    // Rückgabe des Trackpoints als String
+    public String toString(){
+        return "id: " + this.id + " Location: " +  this.location.toString() + " Timestamp: " + this.timestamp.toString();
+    }
+
+   // toleranceRangeTime in Millisekunden, testet ob 2 Trackpoints in Zeit und Ort bis auf Toleranzgrenzen übereinstimmen
+   public boolean equals(long toleranceRangeTime, double toleranceRangeLocation, Trackpoint trackpoint) {
+        double distanceLocation = this.location.distanceTo(trackpoint.getLocation());
+        long distanceTime = Math.abs(this.timestamp.getTime() - trackpoint.getTimestamp().getTime());
+        return (distanceLocation <= toleranceRangeLocation && distanceTime <= toleranceRangeTime);
+    }
 }
--- LocationMapping/TrackpointList.java
+++ LocationMapping/TrackpointList.java
@@ -1,3 +1,30 @@
-class Trackpoint {
+class TrackpointList implements Iterable<Trackpoint>, Iterable<Pair<Location, Integer>> { //?? iterators seltsam
+    // Attribute
+    ArrayList<Trackpoint> trackpointList;
+    Hashtable<Location, Integer> locationFrequencies;
+    int length;
+    double locationAccuracy;
+    String name; // oder int id
+    Trackpoint first;
+    Trackpoint last;
 
+    // Konstruktor
+    TrackpointList(); // erstellt leere liste
+
+    // Methoden
+    getFrequency(double longitude, double lattitude);
+    getFrequency(Location location);
+
+    addTrackpoint(Trackpoint tp); // muss auch dictionary updaten
+    deleteTrackpoint(Trackpoint tp);
+
+    Trackpoint find(Timestamp timestamp);
+    Trackpoint find(Location location);
+    Trackpoint find(double longitude, double lattitude);
+
+    public Iterator<Trackpoint> iterator(){
+        return new timeOrderedIterator(first);
+    }
+    public Iterator<Trackpoint> timeOrderedIterator(Trackpoint start);
+    public Iterator<Pair<Location, Integer>> locationFrequencyIterator();
 }
