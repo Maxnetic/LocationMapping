3aceb663fdd3f33687172abaf3ae15bc5e753e19
MrLoh <flojoto@hotmail.com>
2015-03-17 17:41:15 +0100

    add Specification for TrackpointList
    Former-commit-id: 1d95bb9ef92ab9074c304236c1ee3311cb4645b3

--- LocationMapping/Trackpoint.java
+++ LocationMapping/Trackpoint.java
@@ -1,5 +1,5 @@
-import java.lang.Object
-
+import de.fhpotsdam.unfolding.geo.Location;
+import java.sql.Timestamp;
 
 class Trackpoint {
     // Attribute
@@ -29,19 +29,19 @@ class Trackpoint {
     /**
     * Konstruktor für
     *
-    * @param int id (optional, default = 0): Identifikationsnummer für Datensatz
-    * @param Timestamp timestamp: Timestamp Objekt mit
-    * @param Location location:
+    * @param id [int] (optional, default = 0): Identifikationsnummer für Datensatz
+    * @param Timestamp timestamp: Timestamp Objekt mit Datum und Zeit
+    * @param Location location: Location Object
     * @param String service (optional, default = ""):
     * @return Objekt vom Typ Trackpoint
     */
-    public Trackpoint(int id, Timestamp timestamp, Location location, String service;){
+    public Trackpoint(int id, Timestamp timestamp, Location location, String service) {
         this.id = id;
         this.timestamp = timestamp;
         this.location = new Location(location);
         this.service = service;
     }
-    public Trackpoint(Timestamp timestamp, Location location){
+    public Trackpoint(Timestamp timestamp, Location location) {
         this.timestamp = timestamp;
         this.location = new Location(location);
     }
@@ -87,20 +87,20 @@ class Trackpoint {
     }
 
     // Test, ob 2 Trackpoints bis auf die angegebene Toleranzgrenze übereinstimmen
-    public boolean equalLocation(double toleranceRange, Trackpoint trackpoint){
-        double distance = this.location.distanceTo(trackpoint.getLocation());
-        return (distance <= toleranceRange);
-    }
+    // public boolean equalLocation(double toleranceRange, Trackpoint trackpoint){
+    //     double distance = this.location.distanceTo(trackpoint.getLocation());
+    //     return (distance <= toleranceRange);
+    // }
 
     // Rückgabe des Trackpoints als String
-    public String toString(){
-        return "id: " + this.id + " Location: " +  this.location.toString() + " Timestamp: " + this.timestamp.toString();
-    }
+    // public String toString(){
+    //     return "id: " + this.id + " Location: " +  this.location.toString() + " Timestamp: " + this.timestamp.toString();
+    // }
 
    // toleranceRangeTime in Millisekunden, testet ob 2 Trackpoints in Zeit und Ort bis auf Toleranzgrenzen übereinstimmen
-   public boolean equals(long toleranceRangeTime, double toleranceRangeLocation, Trackpoint trackpoint) {
-        double distanceLocation = this.location.distanceTo(trackpoint.getLocation());
-        long distanceTime = Math.abs(this.timestamp.getTime() - trackpoint.getTimestamp().getTime());
-        return (distanceLocation <= toleranceRangeLocation && distanceTime <= toleranceRangeTime);
-    }
+   // public boolean equals(long toleranceRangeTime, double toleranceRangeLocation, Trackpoint trackpoint) {
+   //      double distanceLocation = this.location.distanceTo(trackpoint.getLocation());
+   //      long distanceTime = Math.abs(this.timestamp.getTime() - trackpoint.getTimestamp().getTime());
+   //      return (distanceLocation <= toleranceRangeLocation && distanceTime <= toleranceRangeTime);
+   //  }
 }
--- LocationMapping/TrackpointList.java
+++ LocationMapping/TrackpointList.java
@@ -1,30 +1,90 @@
-class TrackpointList implements Iterable<Trackpoint>, Iterable<Pair<Location, Integer>> { //?? iterators seltsam
+class TrackpointList implements Iterable<Trackpoint> {
+
     // Attribute
     ArrayList<Trackpoint> trackpointList;
     Hashtable<Location, Integer> locationFrequencies;
-    int length;
-    double locationAccuracy;
-    String name; // oder int id
-    Trackpoint first;
-    Trackpoint last;
+    int length = 0;
 
-    // Konstruktor
-    TrackpointList(); // erstellt leere liste
+    // Konstruktor erstellt leere liste
+    TrackpointList() {
+         trackpointList = new ArrayList<Trackpoints>();
+     }
 
     // Methoden
-    getFrequency(double longitude, double lattitude);
-    getFrequency(Location location);
 
-    addTrackpoint(Trackpoint tp); // muss auch dictionary updaten
-    deleteTrackpoint(Trackpoint tp);
+    //gibt Anzahl der Trackpoints in der Nähe dieser Location an
+    int getFrequency(Location location) {
+        return locationFrequencies.get(location);
+    }
 
-    Trackpoint find(Timestamp timestamp);
-    Trackpoint find(Location location);
-    Trackpoint find(double longitude, double lattitude);
+     // testet ob schon nach Zeit sortiert ist, "vorwärts"-Sortierung
+    boolean isSortedByTime(){
+        for (int i = 0; i < this.length-1, i++){
+            if (trackpointList.get(i).getTimestamp().after(trackpointList.get(i+1).getTimestamp())){
+                    return false;
+            }
+        }
+        return true;
+    }
 
+    void sortByTime();
+
+
+
+    int getFrequency(Location location){
+        return locationFrequencies.get(location);
+    };
+
+    // fügt Trackpoint am Ende der Liste ein
+    addTrackpoint(Trackpoint tp){
+        Location trackpointLocation = tp.getLocation();
+        trackpointList.add(tp);
+        lenghth++;
+        // update Hashtable, wenn Wert vorhanden, Wert erhöhen (schönere Variante?),
+        // sonst neu anlegen
+        if (locationFrequencies.contains(trackpointLocation)){
+            locationFrequencies.put(trackpointLocation, locationFrequencies.get(trackpointLocation)+1)
+        } else {
+            locationFrequencies.put(trackpointLocation, 1);
+        }
+    };
+
+
+    //löscht Trackpoint aus der Liste
+    deleteTrackpoint(Trackpoint tp){
+        Location trackpointLocation = tp.getLocation();
+        trackpointList.remove(tp);
+        length--;
+        // Falls Trackpoint nur noch mit Häufigkeit 1 vorhanden, Trackpoint löschen
+        // sonst Wert um 1 verringern
+        if (locationFrequencies.get(trackpointLocation) == 1){
+            locationFrequencies.remove(trackpointLocation);
+        } else {
+            locationFrequencies.put(trackpointLocation, locationFrequencies.get(trackpointLocation)-1)
+        }
+    };
+
+    // finde Trackpoint nach Timestamp
+    Trackpoint find(Timestamp timestamp){
+
+    }
+
+    // finde Trackpoint nach Ort
+    Trackpoint find(Location location){
+
+    }
+
+    // Iterator über TrackpointList ohne Startwert
     public Iterator<Trackpoint> iterator(){
         return new timeOrderedIterator(first);
     }
-    public Iterator<Trackpoint> timeOrderedIterator(Trackpoint start);
-    public Iterator<Pair<Location, Integer>> locationFrequencyIterator();
+
+    // Iterator über TrackpointList mit Startwert
+    public Iterator<Trackpoint> iterator(Trackpoint start);
+
+    Hashtable<Location, Integer> getLocationFrequencies(){
+        return locationFrequencies;
+    }
 }
+
+
