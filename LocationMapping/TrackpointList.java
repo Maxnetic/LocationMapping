a8b0cc4768e4a7bd55062be6232941e7b8265e79
SanWade <big-sister2000@hotmail.de>
2015-03-18 13:57:11 +0100

    sort-funktion, doppelte funktion gelöscht, teilweise javadoc
    Former-commit-id: e91f31fa90b67892adcd0fa8996643ffce6c56f9

--- LocationMapping/TrackpointList.java
+++ LocationMapping/TrackpointList.java
@@ -1,10 +1,23 @@
 class TrackpointList implements Iterable<Trackpoint> {
 
     // Attribute
+	/**
+	 * ArrayList trackpointList enthaelt einzelne Trackpoints
+	 */
     ArrayList<Trackpoint> trackpointList;
+    /**
+     * Hashtable locationFrequencies enthaelt Häufigkeiten der einzelnen Trackpoints
+     */
     Hashtable<Location, Integer> locationFrequencies;
+    /**
+     * Länge der trackpointListe
+     */
     int length = 0;
-
+    
+    /**
+     * Konstruktor fuer
+     * @return Objekt vom Typ ArrayList<Trackpoint>
+     */
     // Konstruktor erstellt leere liste
     TrackpointList() {
          trackpointList = new ArrayList<Trackpoints>();
@@ -13,11 +26,20 @@ class TrackpointList implements Iterable<Trackpoint> {
     // Methoden
 
     //gibt Anzahl der Trackpoints in der Naehe dieser Location an
+    /**
+     * Gibt aus wie haufig ein Ort als Trackpoint vorkommt
+     * @param Location location : Location Objekt
+     * @return Objekt vom Typ Int
+     */
     int getFrequency(Location location) {
         return locationFrequencies.get(location);
     }
-
-     // testet ob schon nach Zeit sortiert ist, "vorwaerts"-Sortierung
+    
+    // testet ob schon nach Zeit sortiert ist, "vorwaerts"-Sortierung
+    /**
+     * Testet, ob eine Liste bereits aufsteigend nach Datum sortiert ist
+     * @return Objekt vom Typ Boolean
+     */
     boolean isSortedByTime(){
         for (int i = 0; i < this.length-1, i++){
             if (this.get(i).getTimestamp().after(this.get(i+1).getTimestamp())){
@@ -27,15 +49,30 @@ class TrackpointList implements Iterable<Trackpoint> {
         return true;
     }
 
-    void sortByTime();
-
-
-
-    int getFrequency(Location location){
-        return locationFrequencies.get(location);
+    // sortiert Trackpoints in der Liste nach Zeit mit Insertionsort
+    /**
+     * Sortiert Liste nach Zeit
+     */
+    void sortByTime(){
+    	for (int i = 1, i < this.length, i++){
+    		Trackpoint key = this.get(i);
+    		int k = i-1;
+    		while ( k >= 0 && (this.get(k).getTimestamp().compareTo(key.getTimestamp()) > 0)){
+    			this.set(k+1,this.get(k));
+    			k = k-1;
+    		}
+    		this.set(k+1,key);
+    	}
     }
 
+
+    
+
     // fuegt Trackpoint am Ende der Liste ein
+    /**
+     * Testet, ob eine Liste bereits aufsteigend nach Datum sortiert ist
+     * @param Trackpoint tp : Trackpoint Objekt
+     */
     addTrackpoint(Trackpoint tp){
         Location trackpointLocation = tp.getLocation();
         trackpointList.add(tp);
@@ -46,6 +83,9 @@ class TrackpointList implements Iterable<Trackpoint> {
             locationFrequencies.put(trackpointLocation, locationFrequencies.get(trackpointLocation)+1);
         } else {
             locationFrequencies.put(trackpointLocation, 1);
+        }
+        if(!(this.isSorted())){
+        	this.sortByTime();
         }
     }
 
