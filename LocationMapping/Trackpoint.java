f1b0b162f34014066076b60f9d0d542f3c06ef72
MrLoh <flojoto@hotmail.com>
2015-03-18 12:08:08 +0100

    add Documentation for Trackpoint
    Former-commit-id: d651df1d03fc103d1a1e7d12356d2dde4417cf82

--- LocationMapping/Trackpoint.java
+++ LocationMapping/Trackpoint.java
@@ -1,6 +1,9 @@
 import de.fhpotsdam.unfolding.geo.Location;
 import java.sql.Timestamp;
 
+/**
+*
+*/
 class Trackpoint {
     // Attribute
     /**
@@ -8,34 +11,33 @@ class Trackpoint {
     */
     private Location location;
     /**
-    * Timestamp
+    * Zeit- und Datumsinformation, enthält long mit UNIX timestamp in ms
     */
     private Timestamp timestamp;
     /**
-    * Identifikationsnummer für den Datensatz, per Default auf 0 gesetzt
+    * Identifikationsnummer für den Datensatz (default = 0)
     */
     private int id = 0;
     /**
-    * Label, welcher von User festgesetzt werden kann, leer bei default
+    * Label, welches von User festgesetzt werden kann (default = "")
     */
     private String label = "";
     /**
-    * Service Information
+    * Information über den benutzten Handy Service (default = "")
     */
     private String service = "";
 
 
-
     /**
-    * Konstruktor für
+    * Konstruktor für Trackpoint Objekte
     *
-    * @param id [int] (optional, default = 0): Identifikationsnummer für Datensatz
-    * @param Timestamp timestamp: Timestamp Objekt mit Datum und Zeit
-    * @param Location location: Location Object
-    * @param String service (optional, default = ""):
-    * @return Objekt vom Typ Trackpoint
+    * @param timestamp [Timestamp]: Zeit- und Datumsinformation für Trackpoint
+    * @param location [Location]: Ortsinformation für Trackpoint
+    * @param id [int]: (default = 0): Identifikationsnummer für Datensatz des Trackpoints
+    * @param service [String]: (default = ""): Handyserviceinformation
+    * @return neues Objekt vom Typ Trackpoint
     */
-    public Trackpoint(int id, Timestamp timestamp, Location location, String service) {
+    public Trackpoint(Timestamp timestamp, Location location, int id, String service) {
         this.id = id;
         this.timestamp = timestamp;
         this.location = new Location(location);
@@ -46,82 +48,151 @@ class Trackpoint {
         this.location = new Location(location);
     }
 
-    // Set ID
+    /**
+    * Setzt die Identifikationsnummer des Trackpoint
+    *
+    * @param id [int]: neue Identifikationsnummer (3 stellige Zahl)
+    */
     void setId(int id){
         this.id = id;
     }
 
-    // Get ID
+    /**
+    * Gibt Identifikationsnummer des Trackpoint aus
+    *
+    * @return [int]: Identifikationsnummer des Trackpoint
+    */
     int getId(){
         return id;
     }
 
-    // Set Label
+    /**
+    * Setzt ein Label für den Trackpoint
+    *
+    * @param label [String]: neues Label für den Trackpoint
+    */
     void setLabel(String label){
         this.label = label;
     }
 
-    // Get Label
+    /**
+    * Gibt das Label des Trackpoint aus
+    *
+    * @return [String]: Label des Trackpoint
+    */
     public String getLabel(){
         return label;
     }
 
-    // Time Methoden
+    /**
+    * Gibt Zeitstempel des Trackpoint aus
+    *
+    * @return [Timestamp]: Zeitstempel des Trackpoint
+    */
     public Timestamp getTimestamp(){
         return timestamp;
     }
 
-    //Ausgabe Datum und Uhrzeit als String
+    /**
+    * Gibt Zeitstempel des Trackpoint in Sekunden aus
+    *
+    * @return [long]: Zeitstempel des Trackpoint
+    */
+    public long getSeconds(){
+        return timestamp.getTime()/1000L;
+    }
+
+    /**
+    * Gibt Datum und Uhrzeit des Trackpoint aus
+    *
+    * @return [String]: String mit Datum und Uhrzeit
+    */
     public String getDateTime(){
         return timestamp.toString();
     }
 
 
 
-    // Location Methoden
+    /**
+    * Gibt Ort des Trackpoint aus
+    *
+    * @return [Location]: Ortsdaten des Trackpoint
+    */
     public Location getLocation(){
         return location;
     }
+
+    /**
+    * Gibt Längengrad des Trackpoint aus
+    *
+    * @return [float]: Längengrad des Trackpoint
+    */
     public float getLatitude(){
         return location.getLat();
     }
+
+    /**
+    * Gibt Breitengrad des Trackpoint aus
+    *
+    * @return [float] Breitengrad des Trackpoint
+    */
     public float getLongitude(){
         return location.getLon();
     }
 
 
-
-    // Rückgabe des Trackpoints als String
-    public String toString(){
-        String out = "Trackpoint(id " + String.format("%03d",this.id) + " | Location " +  this.location.getLat() + " " + this.location.getLon() + " | Timestamp " + this.timestamp;
-        if ( !this.label.isEmpty() )
-            out += " | Label '" + this.label + "'";
-        return out + ")";
-    }
-
-    // Zeit Vergleich und Differenz
-
+    /**
+    * Berechnet zeitlichen Abstand zwischen zwei Trackpoints
+    *
+    * @param trackpoint [Trackpoint]: Trackpoint mit dem Zeit verglichen wird
+    * @return [long]: Zeitdifferenz in Sekunden
+    */
     public long timeDistanceTo(Trackpoint trackpoint){
-        return this.timestamp.getTime() - trackpoint.getTimestamp().getTime();
+        return this.getSeconds() - trackpoint.getSeconds();
     }
 
+    /**
+    * Überprüft ob zwei Trackpoints innerhalb einer Toleranzgrenze gleichzeitig sind
+    *
+    * @param trackpoint [Trackpoint]: Trackpoint mit dem Zeit verglichen wird
+    * @param tolerance [long]: (default = 3s) Toleranzgrenze für Zeitgleichheit in Sekunden
+    * @return [boolean]: Wahrheitswert der Gleichzeitigkeit der Trackpoints innerhalb der Toleranz
+    */
     public boolean equalTime(Trackpoint trackpoint){
-        return equalTime(trackpoint, 3000);
+        return equalTime(trackpoint, 3);
     }
     public boolean equalTime(Trackpoint trackpoint, long tolerance){
         return this.timeDistanceTo(trackpoint) <= tolerance;
     }
 
+    /**
+    * Vergleicht Trackpoints bezüglich ihrer Zeitkoordinate
+    *
+    * @param trackpoint [Trackpoint]: Trackpoint mit dem Zeit verglichen wird
+    * @return [int]: Zahl gleich 0 falls gleich, kleiner 0 falls anderer Trackpoint führe, größer 0 falls anderer Trackpoint später
+    */
     public int compareTimeTo(Trackpoint trackpoint){
         return (int) this.timeDistanceTo(trackpoint);
     }
 
 
-    // Test, ob 2 Trackpoints bis auf die angegebene Toleranzgrenze übereinstimmen
+    /**
+    * Berechnet örtlichen Abstand zwischen zwei Trackpoints
+    *
+    * @param trackpoint [Trackpoint]: Trackpoint zu dem Abstand berechnet wird
+    * @return [double]: Abstand zwischen den Trackpoints in Grad
+    */
     public double locationDistanceTo(Trackpoint trackpoint){
         return this.location.getDistance(trackpoint.getLocation());
     }
 
+    /**
+    * Überprüft ob zwei Trackpoints innerhalb einer Toleranzgrenze am gleichen Ort sind
+    *
+    * @param trackpoint [Trackpoint]: Trackpoint mit dem Ort vergliche wird
+    * @param tolerance [double]: (default = 0.00009˚) Toleranzgrenze für Ortsgleichheit in Grad
+    * @return [boolean]: Wahrheitswert der Ortsgleichheit der Trackpoints innerhalb der Toleranz
+    */
     public boolean equalLocation(Trackpoint trackpoint){
         return equalLocation(trackpoint, 0.00009);
     }
@@ -129,13 +200,33 @@ class Trackpoint {
         return (this.locationDistanceTo(trackpoint) <= tolerance);
     }
 
-
-
-   // toleranceRangeTime in Millisekunden, testet ob 2 Trackpoints in Zeit und Ort bis auf Toleranzgrenzen übereinstimmen
+    /**
+    * Überprüft ob zwei Trackpoints innerhalb einer Toleranzgrenze gleichzeitig und am gleichen Ort sind
+    *
+    * @param trackpoint [Trackpoint]: Trackpoint mit dem vergliche wird
+    * @param tolerance [long]: (default = 3s) Toleranzgrenze für Zeitgleichheit in Sekunden
+    * @param tolerance [double]: (default = 0.00009˚) Toleranzgrenze für Ortsgleichheit in Grad
+    * @return [boolean]: Wahrheitswert der Gleichzeitigkeit und Ortsgleichheit der Trackpoints innerhalb der Toleranzen
+    */
    public boolean equals(Trackpoint trackpoint) {
         return this.equalLocation(trackpoint) && this.equalTime(trackpoint);
     }
     public boolean equals(Trackpoint trackpoint, long timeTolerance, double locationTolerance) {
         return this.equalLocation(trackpoint, locationTolerance) && this.equalTime(trackpoint, timeTolerance);
+    }
+
+
+    /**
+    * Gibt Stringrepräsentation des Trackpoint zurück
+    *
+    * @return [String]: Stringrepräsentation des Trackpoint
+    */
+    public String toString(){
+        String out = "Trackpoint(id " + String.format("%03d",this.id) + " | Location " +  this.location.getLat() + " " + this.location.getLon() + " | Timestamp " + this.timestamp;
+        if ( !this.service.isEmpty() )
+            out += " | Service " + this.service;
+        if ( !this.label.isEmpty() )
+            out += " | Label '" + this.label + "'";
+        return out + ")";
     }
 }
