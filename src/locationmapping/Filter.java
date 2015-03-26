85f828bd3da70675aad9d2f23ab8e2f3968acd07
Sebastian <dardanos@fu-berlin.de>
2015-03-26 10:53:16 +0100

    Compare-Funktion hinzugefügt
    Former-commit-id: b40af3196b8c153ca04794efc2617021fa59f430

--- src/locationmapping/Filter.java
+++ src/locationmapping/Filter.java
@@ -399,4 +399,35 @@ public class Filter{
          }
          return filteredtpl;
       }
+	  
+	  // zeichne Trackpoint nur, wenn beide innerhalb des Radius waren
+      public TrackpointList compareLocation(TrackpointList eingabetpl1, TrackpointList eingabetpl2, int radius){
+      TrackpointList newtpl = new TrackpointList();
+       
+       for (Trackpoint tp1 : eingabetpl1){
+           for (Trackpoint tp2 : eingabetpl2){
+                 if (tp1.locationDistanceTo(tp2.getLocation()) <= radius){
+                     newtpl.add(tp1);
+                     break;
+               }
+             }
+       }
+       return newtpl;
+    }
+  
+	//zeichne Trackpoint nur, wenn Ort und Zeit übereinstimmen
+    public TrackpointList compareLocationAndTime(TrackpointList eingabetpl1, TrackpointList eingabetpl2, int radius, int minutes){
+         TrackpointList newtpl = new TrackpointList();
+         
+         for (Trackpoint tp1 : eingabetpl1){
+             for (Trackpoint tp2 : eingabetpl2){
+                 if (  (tp1.locationDistanceTo(tp2.getLocation()) <= radius) && (tp1.locationDistanceTo(tp2) < minutes * 60) ){
+                     newtpl.add(tp1);
+                     break;
+                 }
+             }
+         }
+         return newtpl;
+  }	  
+	  
 }
