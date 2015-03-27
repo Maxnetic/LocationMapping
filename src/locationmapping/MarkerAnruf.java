c25ead7cee311a12e67eca23e707a8baa500ee33
SanWade <big-sister2000@hotmail.de>
2015-03-27 13:59:28 +0100

    MarkerAnruf, MarkerInternet Doku überarbeitet
    Former-commit-id: 4bf78e9c115201e7ef8919d72f28d7c7859346be

--- src/locationmapping/MarkerAnruf.java
+++ src/locationmapping/MarkerAnruf.java
@@ -4,15 +4,30 @@ import processing.core.PGraphics;
 import de.fhpotsdam.unfolding.geo.Location;
 
 public class MarkerAnruf extends StandardMarker {
-
+	/**
+	* Konstruktor für MarkerAnruf Objekte
+	*
+	* @param location Ort fuer den Marker erstellt werden soll
+	* @return neues Objekt vom Typ MarkerAnruf
+	*/
 	public MarkerAnruf(Location location){
 		super(location);
 	}
-	
+	/**
+	* Konstruktor für MarkerAnruf Objekte
+	*
+	* @param trackpoint Trackpoint fuer den Marker erstellt werden soll
+	* @return neues Objekt vom Typ MarkerAnruf
+	*/
 	public MarkerAnruf(Trackpoint trackpoint) {
 	    super(trackpoint.getLocation());
 	}
-		
+	/**
+	* Zeichnet Marker
+	* @param pg Objekt das gezeichnet werden soll
+	* @param x X-Koordinate
+	* @param y Y-Koordinate
+	*/	
 	public void draw(PGraphics pg, float x, float y){
 		if (!this.isHidden()){
 			pg.pushStyle();
