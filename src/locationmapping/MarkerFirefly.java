e0785d2d878632a02ee06a11d9927c7d42dd92b9
Maximilian G�hner <maxgoe119@googlemail.com>
2015-03-30 12:17:40 +0200

    Firefly Beispiel

---
+++ src/locationmapping/MarkerFirefly.java
@@ -0,0 +1,17 @@
+package locationmapping;
+
+import de.fhpotsdam.unfolding.geo.Location;
+
+public class MarkerFirefly extends StandardMarker {
+
+	public MarkerFirefly(Location location) {
+		super(location);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MarkerFirefly(Trackpoint trackpoint) {
+		super(trackpoint);
+		// TODO Auto-generated constructor stub
+	}
+
+}
